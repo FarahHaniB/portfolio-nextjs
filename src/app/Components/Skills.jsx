1b4c283e7a93a7d3888fa6b2566e3c62082cde8d
FarahHaniB <109828677+FarahHaniB@users.noreply.github.com>
2023-10-14 14:03:13 +0800

    Added skills section

--- src/app/Components/Skills.jsx
+++ src/app/Components/Skills.jsx
@@ -4,16 +4,169 @@ import Image from "next/image";
 
 const Skills = () => {
   return (
-    <section className="text-black w-full lg:h-screen p-2">
+    <section className="text-black w-full p-2">
       <h2 className="text-center text-4xl font-bold mt-4 py-8">Skills</h2>
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-8">
         <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-          <div className="grid grid-rows-2 gap-4">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
             <div className="m-auto">
-                <Image src='/../../../public/Images/SkillsIcon/html.png' width={100} height={100} alt='/'/>
+              <Image
+                src="/Images/SkillsIcon/html-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">HTML</h3>
             </div>
           </div>
         </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/css-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">CSS</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/js-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">JavaScript</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/react-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">React</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/chakraui-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">Chakra UI</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/firebase-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">Firebase</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/tailwind-css-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">Tailwind CSS</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/vscode-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">VS Code</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/nextjs-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">Next.js</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="grid grid-rows-2 gap-4 items-center justify-center">
+            <div className="m-auto">
+              <Image
+                src="/Images/SkillsIcon/ts-icon.png"
+                width={50}
+                height={50}
+                alt="/"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="font-bold">Typescript</h3>
+            </div>
+          </div>
+        </div>
+
       </div>
     </section>
   );
