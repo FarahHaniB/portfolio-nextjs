f6c381b61cf0ab8abf06250565c55e2a31b6b7c5
FarahHaniB <109828677+FarahHaniB@users.noreply.github.com>
2023-10-19 11:11:01 +0800

    Changed to capital letter

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     // './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     // './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './Components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
 
   ],
