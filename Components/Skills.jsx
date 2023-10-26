012f4115fbaf5ac88a70439e188df1115e55babd
FarahHaniB <109828677+FarahHaniB@users.noreply.github.com>
2023-10-26 15:40:09 +0800

    Edited media

--- Components/Skills.jsx
+++ Components/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
   return (
     <section id="skills" className="text-black w-full p-2">
       <h2 className="text-center text-4xl font-bold mt-4 py-8">Skills</h2>
-      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
           <div className="grid grid-rows-2 gap-4 items-center justify-center">
             <div className="m-auto">
